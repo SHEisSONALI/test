1800e48ab271cf2eab6afbbd2d2bc6fd183fdad2
SheisSonali <sonashekhawat11@gmail.com>
2025-09-06 12:06:34 +0530

    Specifires added

---
+++ test/specifier.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int b=10;
+   
+float a=21.3;
+printf("a=%2.2f\n",a);
+printf("decimal value of b=%d\n",b);
+printf("octal value of b=%o\n",b);
+printf("hexadecimal value of b=%x\n",b);
+printf("with g specifier(For float type with the current precision) a=%g\n",b);
+
+}
